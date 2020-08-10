f135a9e14b43bdf9c0cfb0a492afad1b4c392e17
Henrico Bela <henrico.nardelli@hotmail.com>
2020-08-10 11:36:05 -0300

    Adicionado operação de Soma no Programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,22 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!!!");
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Hello World!!!");
+		System.out.println("Digite numeros para soma:");
+		int nmb1 = sc.nextInt();
+		int nmb2 = sc.nextInt();
+		int result = nmb1 + nmb2;
+		
+		System.out.println("numeros somados: " + result);
+		
+		sc.close();
 	}
 
 }
